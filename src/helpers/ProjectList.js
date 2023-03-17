66aa627fad620ccec9cbe3cd9190fd557437630d
THENISHA-S <thenisha.it20@bitsathy.ac.in>
2023-03-17 11:16:56 +0530

    Dat-6

---
+++ src/helpers/ProjectList.js
@@ -0,0 +1,13 @@
+import deftpatrol from "../assets/deftpatrol.jpg";
+import darkweb from "../assets/darkweb.jpg";
+
+export const ProjectList = [
+    {
+        name:"Deft Patrol",
+        image:deftpatrol,
+    },
+    {
+        name:"Dark Web",
+        image:darkweb,
+    },
+];
--- src/pages/Project.js
+++ src/pages/Project.js
@@ -1,10 +1,17 @@
 import React from "react";
-
+import ProjectItem from "../components/Projectitem";
+// import darkweb from "../assets/darkweb.jpg";
+// import deftpatrol from "../assets/deftpatrol.jpg";
+import "../styles/projects.css";
+import { ProjectList } from "../helpers/ProjectList";
 const Project = () =>{
     return(
         <div className="project">
             <h1>My personal projects</h1>
-            <div className="projecList"></div>
+            <div className="projectList"></div>
+            {ProjectList.map((project)=>{
+                return <ProjectItem name={project.name} image ={project.image}/> 
+            })}
         </div>
     )
 }
