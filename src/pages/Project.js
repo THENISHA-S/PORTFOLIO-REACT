5f9a3d2dd0a8ba4f193391c9a22d651648c00c1f
THENISHA-S <thenisha.it20@bitsathy.ac.in>
2023-04-11 22:19:51 +0530

    Day-21

--- src/pages/Project.js
+++ src/pages/Project.js
@@ -1,52 +1,43 @@
 import React from "react";
 import "../styles/Project.css";
 import Darkweb from "../assets/darkweb.jpg";
-import Deftpatrol from "../assets/deftpatrol.jpg";
-import {NavLink} from "react-router-dom";
+import Deftpatrol from "../assets/123.png";
 
 const Project = () => {
     return (
-        <div className="work-container">
-            <h1 className="project-heading">Projects</h1>
-            <div className="project-container">
-                <div className="project-card">
-                    <img src={Deftpatrol} alt=""/>
-                    <h2 className="project-title">Deft Patrol Application</h2>
-                    <div className="pro-details">
-                        <p>In our project, we had proposed a mobile application to increase the connectivity in 
-                           between the police itself in a more efficient manner. Our mobile application consists 
-                           of a primary model to spot the crime and inform the committed crime to all other police
-                           officers whoever is using the application. Our mobile application also includes the 
-                           patrol checking system for convenience purposes.</p>
-                        <div className="pro-btns">
-                           <NavLink to="https://github.com/thenisha009/DEFT-PATROL" className="btn">Source</NavLink>
-                        </div>
+        <section id="portfolio">
+           <h1>My recent Works</h1>
+           <div className="container portfolio_container">
+                <article className="portfolio_item">
+                    <div className="portfolio_item image">
+                        <img src={Deftpatrol} alt=""></img>
                     </div>
-                </div>
-            </div>
-            <div className="project-container">
-                <div className="project-card">
-                    <img src={Darkweb} alt="image"/>
-                    <h2 className="project-title">Darkweb Crawling</h2>
-                    <div className="pro-details">
-                        <p>develop a crawler focused on the Tor network, which searches, analyses, and
-                           indexes websites containing drug patterns. The general objective of the work is to develop a search
-                           engine that allows to search the sites with illegal content within the TOR network through crawlers
-                           dedicated to search, identify, and index secret services, black markets and to improve the accuracy
-                           of results of current search engines. </p>
-                        <div className="pro-btns">
-                           <NavLink to="https://github.com/thenisha009/CRAWLING-OF-DARKWEB" className="btn">Source</NavLink>
-                        </div>
+                    <h2>Deftpatrol</h2>
+                    <p>In our project, we had proposed a mobile application to increase the connectivity in 
+                       between the police itself in a more efficient manner. Our mobile application consists 
+                       of a primary model to spot the crime and inform the committed crime to all other police
+                       officers whoever is using the application. Our mobile application also includes the 
+                       patrol checking system for convenience purposes.</p>
+                    <a href="https://github.com/thenisha009/DEFT-PATROL"><button>Github</button></a>
+                </article>
+                
+               
+                <article className="portfolio_item">
+                    <div className="portfolio_item image">
+                        <img src={Darkweb} alt=""></img>
                     </div>
-                </div>
-            </div>
-        </div>
-    );
+                    <h2>Darkweb</h2>
+                    <p>Developed a crawler focused on the Tor network, which searches, analyses, and
+                       indexes websites containing drug patterns. The general objective of the work is to develop a search
+                       engine that allows to search the sites with illegal content within the TOR network through crawlers
+                       dedicated to search, identify, and index secret services, black markets and to improve the accuracy
+                       of results of current search engines. </p>
+                    <a href="https://github.com/thenisha009/CRAWLING-OF-DARKWEB" ><button>Github</button></a>
+                </article>
+           </div>
+        </section>
+);
 }
 
 export default Project;
-
-
-
-
 
